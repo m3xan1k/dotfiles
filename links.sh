9aac785995446b29a97146f3df19064a956f736d
m3xan1k <m3xan1k@duck.com>
2023-05-17 11:12:55 +0300

    reinit

---
+++ links.sh
@@ -0,0 +1,5 @@
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore ~/.gitignore
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
