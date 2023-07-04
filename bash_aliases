5eaf8edd5752a20e4b30abd7b727b1c7695d2dbc
m3xan1k <m3xan1k@duck.com>
2023-07-04 14:19:37 +0300

    aliases

---
+++ bash_aliases
@@ -0,0 +1,4 @@
+alias spell="cd ~/Documents/SPELL/spell && pipenv shell"
+alias gst="git status"
+alias gdf="git diff"
+alias tm="tmux attach -t work || tmux new -s work"
--- links.sh
+++ links.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
