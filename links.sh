8446e1edaa8b9af1ceb85de849aebc5f0432a6fa
m3xan1k <m3xan1k@duck.com>
2023-08-30 15:19:39 +0300

    fix script

--- links.sh
+++ links.sh
@@ -1,5 +1,11 @@
+if [ -f ~/.gitignore ]; then
+    rm ~/.gitignore
+fi
+
 for f in $(ls .); do
     if [ -d $f ]; then
         stow -Rv $f
     fi
 done
+
+ln -s ~/dotfiles/home/.gitignore ~/.gitignore
